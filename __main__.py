c76d88bde9912935ffb66fff08df20b775ab0b95
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-12 20:53:46 +0100

    Implement starting postion as classmethod

--- __main__.py
+++ __main__.py
@@ -14,45 +14,7 @@ def main():
         [[♜, ♞, ♝, ♛, ♚, ♝, ♞, ♜], ...]
     """
 
-    def setup_starting_board() -> list[list[Piece]]:
-        """Return a list of lists representing the starting chess board.
-
-        Returns:
-            8x8 board with pieces in standard starting positions.
-        """
-        piece_order = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
-
-        board = [[None for _ in range(8)] for _ in range(8)]
-
-        board[1] = [Pawn(Color.BLACK, Coordinate(1, col)) for col in range(8)]
-        board[6] = [Pawn(Color.WHITE, Coordinate(6, col)) for col in range(8)]
-
-        board[0] = [
-            piece(Color.BLACK, Coordinate(0, col))
-            for col, piece in enumerate(piece_order)
-        ]
-        board[7] = [
-            piece(Color.WHITE, Coordinate(7, col))
-            for col, piece in enumerate(piece_order)
-        ]
-
-        return board
-
-    board = setup_starting_board()
-    history = [board]
-    player_to_move = Color.WHITE
-    castle_allowed = [Color.WHITE, Color.BLACK]
-    en_passant_square = None
-    halfmove_clock = 0
-
-    board = Board(
-        board,
-        history,
-        player_to_move,
-        castle_allowed,
-        en_passant_square,
-        halfmove_clock,
-    )
+    board = Board.starting_setup()
     for row in board.board:
         row = [char or 0 for char in row]
         print(row)
--- board.py
+++ board.py
@@ -93,6 +93,38 @@ class Board:
         self.en_passant_square = en_passant_square
         self.halfmove_clock = halfmove_clock
 
+    @classmethod
+    def starting_setup(cls) -> Board:
+        """Return a list of lists representing the starting chess board.
+
+        Returns:
+            8x8 board with pieces in standard starting positions.
+        """
+        piece_order = [Rook, Knight, Bishop, Queen, King, Bishop, Knight, Rook]
+
+        board = [[None for _ in range(8)] for _ in range(8)]
+
+        board[1] = [Pawn(Color.BLACK, Coordinate(1, col)) for col in range(8)]
+        board[6] = [Pawn(Color.WHITE, Coordinate(6, col)) for col in range(8)]
+
+        board[0] = [
+            piece(Color.BLACK, Coordinate(0, col))
+            for col, piece in enumerate(piece_order)
+        ]
+        board[7] = [
+            piece(Color.WHITE, Coordinate(7, col))
+            for col, piece in enumerate(piece_order)
+        ]
+
+        return cls(
+            board=board,
+            history=[board.copy()],
+            player_to_move=Color.WHITE,
+            castling_allowed=[Color.WHITE, Color.BLACK],
+            en_passant_square=None,
+            halfmove_clock=0,
+        )
+
     @property
     def repetitions_of_position(self) -> int:
         """Count how many times the current board state has occurred.
