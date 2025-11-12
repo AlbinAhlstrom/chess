aad29494de78129cffbe95a08e3a02b3a1b8a84a
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-12 20:31:47 +0100

    Add piece color tests

--- tests/test_pieces.py
+++ tests/test_pieces.py
@@ -6,6 +6,7 @@ from chess.pieces import Rook, Bishop, Queen, Knight, King, Pawn
 
 def test_rook_moves_and_value():
     rook = Rook(Color.WHITE, Coordinate(4, 4))
+    assert rook.color == Color.WHITE
     moves = rook.moves()
     assert len(moves) == 14
     assert rook.value == 5
@@ -13,6 +14,7 @@ def test_rook_moves_and_value():
 
 def test_bishop_moves_and_value():
     bishop = Bishop(Color.BLACK, Coordinate(4, 4))
+    assert bishop.color == Color.BLACK
     moves = bishop.moves()
     assert len(moves) == 13
     assert bishop.value == 3
@@ -20,6 +22,7 @@ def test_bishop_moves_and_value():
 
 def test_queen_moves_and_value():
     queen = Queen(Color.WHITE, Coordinate(4, 4))
+    assert queen.color == Color.WHITE
     moves = queen.moves()
     assert len(moves) == 27
     assert queen.value == 9
@@ -27,6 +30,7 @@ def test_queen_moves_and_value():
 
 def test_knight_moves_and_value():
     knight = Knight(Color.BLACK, Coordinate(4, 4))
+    assert knight.color == Color.BLACK
     moves = knight.moves()
     assert len(moves) == 8
     assert knight.value == 3
@@ -34,6 +38,7 @@ def test_knight_moves_and_value():
 
 def test_king_moves_and_value():
     king = King(Color.WHITE, Coordinate(4, 4))
+    assert king.color == Color.WHITE
     moves = king.moves()
     assert len(moves) == 8
     assert king.value == float("inf")
@@ -42,8 +47,8 @@ def test_king_moves_and_value():
 def test_pawn_moves_first_and_after_moving():
     pawn = Pawn(Color.WHITE, Coordinate(6, 4))
     assert pawn.has_moved == False
+    assert pawn.color == Color.WHITE
     moves = pawn.moves()
-    assert len(moves) == 2
     assert Coordinate(5, 4) in moves
     assert Coordinate(4, 4) in moves
 
