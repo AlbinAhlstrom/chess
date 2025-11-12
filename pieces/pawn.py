10db3964894e8b981730832260ef475bcccc5da1
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-11-12 19:55:23 +0100

    Change faulty match statement to if

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -18,11 +18,10 @@ class Pawn(Piece):
 
     def moves(self):
         pos = self.position
-        match self.color:
-            case Color.WHITE:
-                move_offsets = (-1,) if self.has_moved else (-1, -2)
-            case Color.BLACK:
-                move_offsets = (1,) if self.has_moved else (1, 2)
+        if self.color == Color.WHITE:
+            move_offsets = (-1,) if self.has_moved else (-1, -2)
+        else:
+            move_offsets = (1,) if self.has_moved else (1, 2)
 
         return {Coordinate(pos.row + move, pos.col) for move in move_offsets}
 
