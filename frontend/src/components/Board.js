2f095cfb6b536d7ccd3f6b36152c99150d9e0646
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-08 01:26:39 +0100

    Add square colors

--- frontend/src/components/Board.js
+++ frontend/src/components/Board.js
@@ -2,15 +2,22 @@ import './Board.css'
 import { fileIntToString } from '../helpers.js'
 
 const Board = () => {
+    const getSquareColor = (i, j) => {
+        let c = "square";
+        c += (i + j) % 2 === 0 ? " square--light" : " square--dark";
+        return c;
+    }
     const ranks = Array(8).fill().map((x, i) => 8-i)
     const files = Array(8).fill().map((x, i) => fileIntToString(i))
 
     return <div className="board">
+        <div className="squares">
         {ranks.map((rank, i) =>
             files.map((file, j) =>
-                <div>{file}{rank}</div>)
+                <div key={file + rank} className={getSquareColor(i, j)}>{file}{rank}</div>)
         )
         }
+        </div>
     </div>
 }
 
