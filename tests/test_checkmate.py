399c0e20b06ebb51a2d3e0f59dc7ac78f121a171
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-08 15:48:57 +0100

    Add checkmate tests

---
+++ tests/test_checkmate.py
@@ -0,0 +1,29 @@
+from chess.board import Board
+from chess.game import Game
+from chess.move import Move
+
+
+def test_checkmate():
+    fen = 'rnbqkbnr/pppp1ppp/8/4p3/6P1/5P2/PPPPP2P/RNBQKBNR b KQkq g3 0 3'
+    board = Board.from_fen(fen)
+    game = Game(board)
+    move = Move.from_uci("d8h4")
+    is_legal, reason = game.is_move_legal(move)
+    assert is_legal, reason
+    game.take_turn(move)
+    assert game.is_over
+    assert game.is_checkmate
+
+
+
+def test_checkmate_on_backrank():
+    fen = 'Q1q1kbnr/2pppppp/8/2n5/8/8/1PPPPPPP/RNBQKBNR w KQk - 0 11'
+    board = Board.from_fen(fen)
+    game = Game(board)
+    move = Move.from_uci("a8c8")
+    is_legal, reason = game.is_move_legal(move)
+    assert is_legal, reason
+    game.take_turn(move)
+    assert game.is_over
+    assert game.is_checkmate
+
