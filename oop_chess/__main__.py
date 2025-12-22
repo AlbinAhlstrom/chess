3a36147bbb6b144c416b916f572029f76f08c976
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-22 13:55:08 +0100

    Use the standardized rules interface in cli game

--- oop_chess/__main__.py
+++ oop_chess/__main__.py
@@ -9,23 +9,23 @@ def main():
     while True:
         print(game.state.board)
 
-        if game.rules.get_game_over_reason(game.state) != GameOverReason.ONGOING:
-            message = "Checkmate" if game.rules.get_game_over_reason(game.state) == GameOverReason.CHECKMATE else "Draw"
+        if game.rules.is_game_over():
+            message = "Checkmate" if game.rules.is_checkmate else "Draw"
             print(message)
             break
 
         print(f"Player to move: {game.state.turn}")
-        print(f"Legal moves: {[str(move) for move in game.rules.get_legal_moves(game.state)]}")
+        print(f"Legal moves: {[str(move) for move in game.rules.get_legal_moves()]}")
         print(f"{game.state.fen=}")
 
         uci_str = input("Enter a move: ")
 
         try:
-            move = Move(uci_str, game.state.turn)
-            if game.rules.validate_move(game.state, move) == MoveLegalityReason.LEGAL:
+            move = Move(uci_str, player_to_move=game.state.turn)
+            if game.rules.validate_move(move) == MoveLegalityReason.LEGAL:
                 game.take_turn(move)
             else:
-                print(game.rules.validate_move(game.state, move).value)
+                print(game.rules.validate_move(move).value)
 
         except (ValueError, IllegalMoveException) as e:
             print(e)
