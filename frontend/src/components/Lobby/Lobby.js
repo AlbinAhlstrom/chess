ec0a1daf3c320a804eb57b443aa32310a1f8ff16
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-24 01:18:31 +0100

    Change own lobby game joinability

--- frontend/src/components/Lobby/Lobby.js
+++ frontend/src/components/Lobby/Lobby.js
@@ -104,7 +104,10 @@ function Lobby() {
                             </tr>
                         </thead>
                         <tbody>
-                            {seeks.map(seek => (
+                            {seeks.map(seek => {
+                                const isMySeek = user && seek.user_id === user.id;
+                                console.log(`Seek ${seek.id}: user=${user?.id}, seek.user_id=${seek.user_id}, isMySeek=${isMySeek}`);
+                                return (
                                 <tr key={seek.id}>
                                     <td>{seek.user_name}</td>
                                     <td>{seek.variant}</td>
@@ -112,13 +115,14 @@ function Lobby() {
                                     <td>
                                         <button 
                                             onClick={() => joinSeek(seek.id)}
-                                            disabled={user && seek.user_id === user.id}
+                                            disabled={isMySeek}
                                         >
                                             Join
                                         </button>
                                     </td>
                                 </tr>
-                            ))}
+                                );
+                            })}
                         </tbody>
                     </table>
                 )}
