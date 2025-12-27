b2eebfe131f6a990d40a2f4bb64ff45e660ab80b
Albin Ahlström <ahlstrom.albin@gmail.com>
2025-12-27 03:19:48 +0100

    ci: add deployment verification marker to about page

--- frontend/src/components/About/About.js
+++ frontend/src/components/About/About.js
@@ -43,6 +43,10 @@ function About() {
                         </a>
                     </div>
                 </section>
+
+                <p style={{ color: '#444', fontSize: '0.8rem', marginTop: '40px' }}>
+                    Server Version: 1.0.2 (Automated Deployment Verified)
+                </p>
             </div>
         </div>
     );
